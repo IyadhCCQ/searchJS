74b8173a15f536732a1c0fb45bb8b3555a39ea50
IyadhCCQ <147079898+IyadhCCQ@users.noreply.github.com>
2023-10-05 13:41:47 -0400

    Update search.js

--- search.js
+++ search.js
@@ -1,1 +1,27 @@
-console.log("srcipt executing");
+document.addEventListener('DOMContentLoaded', function () {
+    var searchInput = document.getElementById('searchInput');
+    var resultsContainer = document.getElementById('results');
+    var items = resultsContainer.getElementsByClassName('checkbox-container');
+
+    // Function to perform search
+    function performSearch(query) {
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+            var text = item.textContent.toLowerCase();
+            if (text.indexOf(query) !== -1) {
+                item.parentElement.style.display = 'block';
+              console.log("I am typing");
+            } else {
+                item.parentElement.style.display = 'none';
+              console.log("I am typing");
+            }
+        }
+    }
+
+    // Add event listener to the search input
+    searchInput.addEventListener('input', function () {
+        var query = searchInput.value.toLowerCase();
+       
+        performSearch(query);
+    });
+});
